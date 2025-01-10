a0bd3f68c84bd99fae99901d938b36c66f6ccac2
Omkarthorat1042 <omkarthorat1042@gmail.com>
2025-01-10 12:29:55 +0530

    created all entites and also mapped by hibernate for all required

---
+++ src/main/java/com/scm/entities/User.java
@@ -0,0 +1,57 @@
+package com.scm.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity(name = "user")
+@Table(name = "users")
+@Data
+public class User {
+
+    @Id
+    private String userId;
+
+    @Column(name = "user_name", nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email; 
+
+    private String password;
+
+    @Column(length = 1000)
+    private String about;
+
+    @Column(length = 1000)
+    private String profilePic;
+
+    private String phoneNumber;
+
+    //information
+    private boolean enabled = false;
+    private boolean emailVerified = false;
+    private boolean phoneVerified = false; 
+
+
+    //self, google, facebook,twitter, linkden, github
+    private Providers provider = Providers.SELF;
+    private String providerId;
+
+
+    //add more fields for the future
+    
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
+    private List<Contact> contacts = new ArrayList<>();
+
+
+
+}
